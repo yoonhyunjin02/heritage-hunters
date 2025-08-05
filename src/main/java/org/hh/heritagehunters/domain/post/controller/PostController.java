ec32eddc6bb7638d3324b5b87e557b25ce18d4b1
SooowanLee <lsy9406@gmail.com>
2025-08-05 13:43:44 +0900

    Feat: PostController 추가

---
+++ src/main/java/org/hh/heritagehunters/domain/post/controller/PostController.java
@@ -0,0 +1,23 @@
+package org.hh.heritagehunters.domain.post.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.hh.heritagehunters.domain.post.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/posts")
+@RequiredArgsConstructor
+@Slf4j
+public class PostController {
+
+  private final PostService postService;
+
+  @GetMapping("/new")
+  public String createPostForm() {
+    // 게시글 작성 폼으로 이동
+    return "features/post/create_post";
+  }
+}
