a17e4d059d0fb4a3063fe25702079d71d636b51a
SooowanLee <lsy9406@gmail.com>
2025-08-05 09:20:33 +0900

    Feat: Post, User 연관관계 추가

--- src/main/java/org/hh/heritagehunters/domain/post/entity/Like.java
+++ src/main/java/org/hh/heritagehunters/domain/post/entity/Like.java
@@ -2,15 +2,17 @@ package org.hh.heritagehunters.domain.post.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hh.heritagehunters.domain.oauth.entity.User;
 
 @Data
 @NoArgsConstructor
@@ -27,6 +29,14 @@ public class Like {
   @Id
   @Column(name = "post_id", nullable = false)
   private Long postId;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "post_id", insertable = false, updatable = false)
+  private Post post;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", insertable = false, updatable = false)
+  private User user;
 
   @Column(name="created_at", nullable = false)
   private LocalDateTime createdAt;
