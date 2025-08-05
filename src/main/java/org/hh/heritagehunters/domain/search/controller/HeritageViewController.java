56d17b22da247316826dd4b8bf1d2023d21c729f
eastdh <eastdh@gmail.com>
2025-08-05 18:28:44 +0900

    Feat: 컨트롤러와 html 연결

--- src/main/java/org/hh/heritagehunters/domain/search/controller/HeritageViewController.java
+++ src/main/java/org/hh/heritagehunters/domain/search/controller/HeritageViewController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/heritages")
+@RequestMapping("/heritage")
 @RequiredArgsConstructor
 public class HeritageViewController {
 
