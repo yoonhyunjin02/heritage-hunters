59bd1a81ee528cd2051239a171538344e62e8192
eastdh <eastdh@gmail.com>
2025-08-05 17:48:17 +0900

    Feat: 국가유산 상세 조회 페이지 추가

---
+++ src/main/java/org/hh/heritagehunters/domain/search/controller/HeritageViewController.java
@@ -0,0 +1,30 @@
+package org.hh.heritagehunters.domain.search.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.hh.heritagehunters.domain.search.dto.HeritageResponse;
+import org.hh.heritagehunters.domain.search.service.HeritageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/heritages")
+@RequiredArgsConstructor
+public class HeritageViewController {
+
+  @Autowired
+  private final HeritageService heritageService;
+
+  /**
+   * 유산 상세 페이지 렌더링
+   */
+  @GetMapping("/{id}")
+  public String getHeritageDetail(@PathVariable Long id, Model model) {
+    HeritageResponse detail = heritageService.getDetail(id);
+    model.addAttribute("heritage", detail);
+    return "/features/search/search_detail";
+  }
+}
