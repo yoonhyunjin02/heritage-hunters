551dad219585fd77f7afe26ae440e40034568be9
eastdh <eastdh@gmail.com>
2025-08-05 17:47:56 +0900

    Chore: 용도에 맞게 rename

--- src/main/java/org/hh/heritagehunters/domain/search/controller/SearchViewController.java
+++ src/main/java/org/hh/heritagehunters/domain/search/controller/SearchViewController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/search")
-public class SearchController {
+public class SearchViewController {
 
   @GetMapping
   public String searchForm(
