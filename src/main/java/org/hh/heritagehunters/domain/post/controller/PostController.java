84d1f97018f70f3ec66085232a499cd3d77bbdb5
SooowanLee <lsy9406@gmail.com>
2025-08-07 16:30:10 +0900

    Feat: 게시글 작성 추가

--- src/main/java/org/hh/heritagehunters/domain/post/controller/PostController.java
+++ src/main/java/org/hh/heritagehunters/domain/post/controller/PostController.java
@@ -1,15 +1,26 @@
 package org.hh.heritagehunters.domain.post.controller;
 
+import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hh.heritagehunters.common.security.CustomUserDetails;
+import org.hh.heritagehunters.domain.post.dto.PostCreateRequest;
+import org.hh.heritagehunters.domain.post.dto.PostCreateResponse;
 import org.hh.heritagehunters.domain.post.entity.Post;
 import org.hh.heritagehunters.domain.post.service.PostService;
 import org.springframework.data.domain.Page;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/posts")
@@ -51,5 +62,34 @@ public class PostController {
               posts.getTotalElements(), posts.getTotalPages(), page + 1);
     
     return "features/post/post_list";
+  }
+
+  /**
+   * 게시글 작성
+   * @return
+   */
+  @PostMapping
+  public String createPost(
+      @AuthenticationPrincipal CustomUserDetails userDetails,
+      @Valid @ModelAttribute PostCreateRequest request,
+      BindingResult bindingResult,
+      @RequestParam(value = "images", required = false) List<MultipartFile> images,
+      RedirectAttributes redirectAttributes) {
+
+    if (bindingResult.hasErrors()) {
+      redirectAttributes.addFlashAttribute("toastType", "error");
+      redirectAttributes.addFlashAttribute("toastMessage", "입력값을 확인해주세요.");
+      return "redirect:/posts";
+    }
+
+    PostCreateResponse response = postService.createPost(userDetails.getUser(), request, images);
+
+    redirectAttributes.addFlashAttribute("toastType", "success");
+    redirectAttributes.addFlashAttribute("toastMessage", response.getMessage());
+    if (response.getPointsEarned() > 0) {
+      redirectAttributes.addFlashAttribute("pointsEarned", response.getPointsEarned());
+    }
+
+    return "redirect:/posts";
   }
 }
