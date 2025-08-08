df833ce502e621b65e29ab77c2b3136b8b688556
SooowanLee <lsy9406@gmail.com>
2025-08-08 11:21:32 +0900

    Feat: 게시글 작성 요청 DTO

--- src/main/java/org/hh/heritagehunters/domain/post/dto/PostCreateRequest.java
+++ src/main/java/org/hh/heritagehunters/domain/post/dto/PostCreateRequest.java
@@ -1,4 +1,23 @@
 package org.hh.heritagehunters.domain.post.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class PostCreateRequest {
+
+  @NotBlank
+  private String content;
+
+  @NotBlank
+  private String location;
+
+  @NotNull
+  private Double lat;
+
+  @NotNull
+  private Double lng;
 }
