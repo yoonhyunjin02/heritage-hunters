bad04408f89405323a1d87f73d97a45c1638fb28
SooowanLee <lsy9406@gmail.com>
2025-08-11 13:55:54 +0900

    Feat: 게시글 수정 DTO

--- src/main/java/org/hh/heritagehunters/domain/post/dto/request/PostUpdateRequestDto.java
+++ src/main/java/org/hh/heritagehunters/domain/post/dto/request/PostUpdateRequestDto.java
@@ -1,4 +1,20 @@
 package org.hh.heritagehunters.domain.post.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostUpdateRequestDto {
+
+  @NotBlank(message = "게시글 내용은 필수입니다.")
+  @Size(max = 300, message = "게시글 내용은 300자를 초과할 수 없습니다.")
+  private String content;
+
+  @NotBlank(message = "위치 정보는 필수입니다.")
+  private String location;
 }
