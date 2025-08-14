1ce1653f804e7a6c099a5b5790e7a77d0053818c
eastdh <eastdh@gmail.com>
2025-08-14 14:48:39 +0900

    Feat: 프로필 페이지에 필요한 백엔드 코드 추가

--- src/main/java/org/hh/heritagehunters/domain/search/repository/HeritageRepository.java
+++ src/main/java/org/hh/heritagehunters/domain/search/repository/HeritageRepository.java
@@ -1,14 +1,21 @@
 package org.hh.heritagehunters.domain.search.repository;
 
+import java.util.List;
 import org.hh.heritagehunters.domain.search.entity.Heritage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.Query;
 
-@Repository
-public interface HeritageRepository
-    extends JpaRepository<Heritage, Long>,
-    JpaSpecificationExecutor<Heritage> {
+public interface HeritageRepository extends JpaRepository<Heritage, Long>, JpaSpecificationExecutor<Heritage> {
 
+  interface StampProjection {
+
+    Long getId();
+
+    String getName();
+
+    String getThumbnailUrl(); // Heritage.thumbnailUrl 필드 기반
+  }
+
+  @Query("select h.id as id, h.name as name, h.thumbnailUrl as thumbnailUrl from Heritage h")
+  List<StampProjection> findAllForStamp();
 }
-
