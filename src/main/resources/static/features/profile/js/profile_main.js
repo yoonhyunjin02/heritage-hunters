366d150ae3b61c5efde2e78ed01b41af271f02b2
eastdh <eastdh@gmail.com>
2025-08-21 23:42:37 +0900

    Rename: 파일 이름 컨벤션에 맞춰서 변경

--- src/main/resources/static/features/profile/js/profile_main.js
+++ src/main/resources/static/features/profile/js/profile_main.js
@@ -1,7 +1,7 @@
 import initTabs from "./tabs.js";
 import initInfiniteScroll from "./infinite_scroller.js";
 import initPostModal from "./profile_post_modal.js";
-import initProfileEdit from "./profileEdit.js";
+import initProfileEdit from "./profile_edit.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initTabs();
