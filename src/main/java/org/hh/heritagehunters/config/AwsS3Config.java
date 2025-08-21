e08b8117dd3ccd993fb3b8dc0cc707f500c11f3d
yoonhyunjin <hyeonjin-hyunjin@naver.com>
2025-08-21 19:10:24 +0900

    Fix: AwsS3Config 클래스 수정

--- src/main/java/org/hh/heritagehunters/config/AwsS3Config.java
+++ src/main/java/org/hh/heritagehunters/config/AwsS3Config.java
@@ -3,18 +3,23 @@ package org.hh.heritagehunters.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3ClientBuilder;
 
 @Configuration
 public class AwsS3Config {
 
-  @Value("${cloud.aws.credentials.access-key}")
+  // prod에서는 빈값(미설정)이어도 되게 기본값을 ""로 둠
+  @Value("${cloud.aws.credentials.access-key:}")
   private String accessKey;
 
-  @Value("${cloud.aws.credentials.secret-key}")
+  @Value("${cloud.aws.credentials.secret-key:}")
   private String secretKey;
 
   @Value("${cloud.aws.region.static}")
@@ -22,11 +27,18 @@ public class AwsS3Config {
 
   @Bean
   public S3Client s3Client() {
-    AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
+    S3ClientBuilder builder = S3Client.builder().region(Region.of(region));
 
-    return S3Client.builder()
-        .region(Region.of(region))
-        .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
-        .build();
+    // 로컬/개발에선 키가 있으면 그걸 사용, 없으면 EC2 Role 등 기본 체인 사용
+    if (StringUtils.hasText(accessKey) && StringUtils.hasText(secretKey)) {
+      AwsCredentialsProvider staticCreds =
+          StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey));
+      builder.credentialsProvider(staticCreds);
+    } else {
+      // EC2 IAM Role, 환경변수, 프로파일 등 기본 자격증명 체인
+      builder.credentialsProvider(DefaultCredentialsProvider.create());
+    }
+
+    return builder.build();
   }
 }
